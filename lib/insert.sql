b703925d06c4eb93aacc9c80178b8d9a5db69d65
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-15 05:37:00 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'F', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'black', 'nice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'M', 'brown', 'charismatic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'F', 'striped', 'introvert', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'striped', 'kind', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 10, 'F', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'brown', 'cranky', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, 'F', 'black', 'combative', 1);
